7ec5d338deb9c1aa66e00e7b502d0602e2a3f0ce
Jesse Sibley <inounomemynameisjesse@gmail.com>
2022-02-22 15:47:43 +0000

    Fix loading view

--- components/repository-view.tsx
+++ components/repository-view.tsx
@@ -6,7 +6,7 @@ import { useFavourites } from "../lib/favourites";
 
 export default function RepositoryView() {
   const [repositories, setRepositories] = useState<Respository[] | null>(null);
-  const [error, setError] = useState<string | null>("Something went wrong");
+  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [language, setLanguage] = useState<string>("*");
   const [isFilterActive, setIsFilterActive] = useState<boolean>(false);
@@ -23,65 +23,57 @@ export default function RepositoryView() {
     });
   }, [language]);
 
-  if (loading) {
-    return (
-      <div className="flex min-h-[300px] items-center justify-center">
-        <span>Loading...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return <span className="text-red-500">Error: {error}</span>;
-  }
-
   return (
     <section>
-      {repositories && (
-        <div>
-          <div className="mb-4 flex items-center justify-between">
-            <label className="inline-flex items-center space-x-2">
-              <input
-                type="checkbox"
-                checked={isFilterActive}
-                onChange={(event) => setIsFilterActive(event.target.checked)}
-                className="filter-checkbox rounded-md"
-              />
-              <span>Filter by favourites</span>
-            </label>
-            <label className="inline-flex items-center space-x-2">
-              <span className="block">Language</span>
-              <select
-                className="language-select rounded-md"
-                value={language}
-                onChange={(event) => {
-                  setLoading(true);
-                  setLanguage(event.target.value);
-                }}
-              >
-                <option value="*">Any</option>
-                <option value="javascript">JavaScript</option>
-                <option value="typescript">TypeScript</option>
-                <option value="python">Python</option>
-                <option value="go">Go</option>
-              </select>
-            </label>
-          </div>
-          <ol className="space-y-4">
-            {repositories
-              .filter((repository) =>
-                isFilterActive ? favourites?.includes(repository.id) : true
-              )
-              .map((repository) => (
-                <li key={repository.id}>
-                  <RepositoryCard
-                    repository={repository}
-                    isFavourite={favourites.includes(repository.id)}
-                  />
-                </li>
-              ))}
-          </ol>
+      <div className="mb-4 flex items-center justify-between">
+        <label className="inline-flex items-center space-x-2">
+          <input
+            type="checkbox"
+            checked={isFilterActive}
+            onChange={(event) => setIsFilterActive(event.target.checked)}
+            className="filter-checkbox rounded-md"
+          />
+          <span>Filter by favourites</span>
+        </label>
+        <label className="inline-flex items-center space-x-2">
+          <span className="block">Language</span>
+          <select
+            className="language-select rounded-md"
+            value={language}
+            onChange={(event) => {
+              setLoading(true);
+              setLanguage(event.target.value);
+            }}
+          >
+            <option value="*">Any</option>
+            <option value="javascript">JavaScript</option>
+            <option value="typescript">TypeScript</option>
+            <option value="python">Python</option>
+            <option value="go">Go</option>
+          </select>
+        </label>
+      </div>
+      {loading && (
+        <div className="flex min-h-[300px] items-center justify-center">
+          <span>Loading...</span>
         </div>
+      )}
+      {error && <span className="text-red-500">Error: {error}</span>}
+      {!loading && repositories && (
+        <ol className="space-y-4">
+          {repositories
+            .filter((repository) =>
+              isFilterActive ? favourites?.includes(repository.id) : true
+            )
+            .map((repository) => (
+              <li key={repository.id}>
+                <RepositoryCard
+                  repository={repository}
+                  isFavourite={favourites.includes(repository.id)}
+                />
+              </li>
+            ))}
+        </ol>
       )}
     </section>
   );
